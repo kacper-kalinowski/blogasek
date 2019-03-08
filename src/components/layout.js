5fb6e39c04588a6ed47bfe511a062cd4c2a05456
dmarkiewicz <dariusz.markiewicz4@gmail.com>
2019-03-08 13:00:02 +0100

    Reset styles, remove unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,43 +7,21 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import "./reset.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+      backgroundColor: "rgb(34, 38, 41)",
+    }}
+  >
+    {children}
+  </div>
 )
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1
+      style={{
+        fontSize: "40px",
+        fontWeight: "bold",
+        margin: "10px",
+        color: "#FFE400",
+      }}
+    >
+      Kacper Kalinowski
+    </h1>
+    <h2
+      style={{
+        fontSize: "28px",
+        fontWeight: "bold",
+        margin: "8px",
+        color: "hsl(0,0%,90%)",
+      }}
+    >
+      Trener personalny
+    </h2>
+    <h3
+      style={{
+        fontSize: "22px",
+        fontWeight: "bold",
+        margin: "3px",
+        color: "hsl(0,0%,90%)",
+      }}
+    >
+      Coming soon...
+    </h3>
   </Layout>
 )
 
