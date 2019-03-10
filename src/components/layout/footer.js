6678dd0b89eb12430d66cb682d4d95a4d1c8f1ef
dmarkiewicz <dariusz.markiewicz4@gmail.com>
2019-03-10 20:11:24 +0100

    Basic layout

---
+++ src/components/layout/footer.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Footer = () => <footer>FOOTER</footer>
+
+export default Footer
---
+++ src/components/layout/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Header from "./header"
+import Footer from "./footer"
+import "./styles/reset.css"
+import "./styles/layout.scss"
+
+const Layout = ({ children }) => (
+  <div className="site">
+    <Header />
+    <main>{children}</main>
+    <Footer />
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
